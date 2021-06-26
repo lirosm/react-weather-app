da373f6c35c1fe4df6b2048a1fc2cc692a296947
Liridona <liridonaosm@gmail.com>
2021-06-26 12:22:14 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import "./App.css";
 
 export default function App() {
   return <div className="App">
+<div className="container">  
+
     <h1>Weather App</h1>
   <footer>
     This project was coded by Liridona Osmani and is open-sourced on {""}
@@ -12,7 +14,8 @@ export default function App() {
 
   </a>
   </footer>
-  </div>;
+  </div>
+</div>;
   
 }
 
