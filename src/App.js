1b0e80fd0bfb644401b86a68079038027e85cef4
Liridona <liridonaosm@gmail.com>
2021-06-26 11:55:36 +0200

    added github repo URL

--- src/App.js
+++ src/App.js
@@ -3,7 +3,16 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return <div className="App">
+    <h1>Weather App</h1>
+  <footer>
+    This project was coded by Liridona Osmani and is open-sourced on {""}
+  <a href="https://github.com/lirosm/react.app.weather" target="_blank"
+  > GitHub
+
+  </a>
+  </footer>
+  </div>;
   
 }
 
