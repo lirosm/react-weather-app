ed5de2d0b2a895f11cc66e4a10984943cb35909a
Liridona <liridonaosm@gmail.com>
2021-06-26 21:50:15 +0200

    added costom icons

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherInfo(props) {
     return  (
@@ -7,7 +8,7 @@ export default function WeatherInfo(props) {
     <h1>{props.data.city}</h1>
         <ul>
             <li>
-                <formattedDate date={props.data.date} />
+             <FormattedDate date={props.data.date} />
             </li>
             <li className ="text-capitalize">{props.data.description}
             </li>
@@ -15,12 +16,11 @@ export default function WeatherInfo(props) {
         <div className="row mt-3">
             <div className="col-6">
                 <div className="clearfix">
-        <img 
-        src={props.data.iconUrl}
-        alt={props.data.description}
-        className="float-left"
-        />
-        <div className="float-left">
+                 <div className="float-left">
+                    <WeatherIcon code={props.data.icon} />
+                </div>
+     
+     <div className="float-left">
         <span className="temperature">
         {Math.round(props.data.temperature)}
         </span>
