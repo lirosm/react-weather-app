ab04c3efa1b1187a330a4a64543472c293afb696
Liridona <liridonaosm@gmail.com>
2021-06-29 18:58:54 +0200

    made new component, weatherforecastday

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import WeatherInfo from "./WeatherInfo";
+import WeatherForecast from "./WeatherForecast";
 import axios from "axios";
 import "./Weather.css";
 
@@ -10,6 +11,7 @@ export default function Weather(props) {
 function handleResponse(response){
     setWeatherData({
         ready:true,
+        coordinates: response.data.coord,
         temperature: response.data.main.temp,
         humidity: response.data.main.humidity, 
         date: new Date(response.data.dt*1000), 
@@ -65,7 +67,7 @@ if (weatherData.ready){
          </div>
         </form>
         <WeatherInfo  data={weatherData}/>
-
+    <WeatherForecast coordinates={weatherData.coordinates} />
  </div>
 );   
 } else {
